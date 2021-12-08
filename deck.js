6291c6cc6e1cd66016212eb7ee483848de26f403
Pelle Lundgren <pelle.lundgren@frankdigital.se>
2021-12-08 08:31:44 +0100

    Add a complete deck

--- deck.js
+++ deck.js
@@ -1,18 +1,226 @@
 export const deck = [
   {
-    rank: 1,
-    suite: "♠",
-  },
-  {
-    rank: 2,
-    suite: "♥",
-  },
-  {
-    rank: 3,
-    suite: "♦",
-  },
-  {
-    rank: 4,
     suite: "♣",
+    rank: 1,
+  },
+  {
+    suite: "♣",
+    rank: 2,
+  },
+  {
+    suite: "♣",
+    rank: 3,
+  },
+  {
+    suite: "♣",
+    rank: 4,
+  },
+  {
+    suite: "♣",
+    rank: 5,
+  },
+  {
+    suite: "♣",
+    rank: 6,
+  },
+  {
+    suite: "♣",
+    rank: 7,
+  },
+  {
+    suite: "♣",
+    rank: 8,
+  },
+  {
+    suite: "♣",
+    rank: 9,
+  },
+  {
+    suite: "♣",
+    rank: 10,
+  },
+  {
+    suite: "♣",
+    rank: "Knight",
+  },
+  {
+    suite: "♣",
+    rank: "Queen",
+  },
+  {
+    suite: "♣",
+    rank: "King",
+  },
+  {
+    suite: "♣",
+    rank: "Ace",
+  },
+  {
+    suite: "♦",
+    rank: 1,
+  },
+  {
+    suite: "♦",
+    rank: 2,
+  },
+  {
+    suite: "♦",
+    rank: 3,
+  },
+  {
+    suite: "♦",
+    rank: 4,
+  },
+  {
+    suite: "♦",
+    rank: 5,
+  },
+  {
+    suite: "♦",
+    rank: 6,
+  },
+  {
+    suite: "♦",
+    rank: 7,
+  },
+  {
+    suite: "♦",
+    rank: 8,
+  },
+  {
+    suite: "♦",
+    rank: 9,
+  },
+  {
+    suite: "♦",
+    rank: 10,
+  },
+  {
+    suite: "♦",
+    rank: "Knight",
+  },
+  {
+    suite: "♦",
+    rank: "Queen",
+  },
+  {
+    suite: "♦",
+    rank: "King",
+  },
+  {
+    suite: "♦",
+    rank: "Ace",
+  },
+  {
+    suite: "♥",
+    rank: 1,
+  },
+  {
+    suite: "♥",
+    rank: 2,
+  },
+  {
+    suite: "♥",
+    rank: 3,
+  },
+  {
+    suite: "♥",
+    rank: 4,
+  },
+  {
+    suite: "♥",
+    rank: 5,
+  },
+  {
+    suite: "♥",
+    rank: 6,
+  },
+  {
+    suite: "♥",
+    rank: 7,
+  },
+  {
+    suite: "♥",
+    rank: 8,
+  },
+  {
+    suite: "♥",
+    rank: 9,
+  },
+  {
+    suite: "♥",
+    rank: 10,
+  },
+  {
+    suite: "♥",
+    rank: "Knight",
+  },
+  {
+    suite: "♥",
+    rank: "Queen",
+  },
+  {
+    suite: "♥",
+    rank: "King",
+  },
+  {
+    suite: "♥",
+    rank: "Ace",
+  },
+  {
+    suite: "♠",
+    rank: 1,
+  },
+  {
+    suite: "♠",
+    rank: 2,
+  },
+  {
+    suite: "♠",
+    rank: 3,
+  },
+  {
+    suite: "♠",
+    rank: 4,
+  },
+  {
+    suite: "♠",
+    rank: 5,
+  },
+  {
+    suite: "♠",
+    rank: 6,
+  },
+  {
+    suite: "♠",
+    rank: 7,
+  },
+  {
+    suite: "♣",
+    rank: 8,
+  },
+  {
+    suite: "♠",
+    rank: 9,
+  },
+  {
+    suite: "♠",
+    rank: 10,
+  },
+  {
+    suite: "♠",
+    rank: "Knight",
+  },
+  {
+    suite: "♠",
+    rank: "Queen",
+  },
+  {
+    suite: "♠",
+    rank: "King",
+  },
+  {
+    suite: "♠",
+    rank: "Ace",
   },
 ];
