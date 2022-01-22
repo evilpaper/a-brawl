a40b5a6e31342f4ad746500edc095fb9ac7a26a4
Pelle Lundgren <pellburt@gmail.com>
2022-01-22 18:59:20 +0100

    Filter out and update status of selected card

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = document.querySelector("#root");
 
 function prepareDeck(array) {
   return array.map((item) => {
-    return { ...item, played: "false" };
+    return { ...item, played: false };
   });
 }
 
@@ -41,7 +41,9 @@ function drawGame(store) {
   app.innerHTML = "";
 
   for (const card of store.round) {
-    html += `<button class="card" data-suite="${card.suite}" data-rank="${card.rank}">
+    html += `<button class="card ${card.played ? "played" : ""}" data-card="${
+      card.suite + card.rank.toString()
+    }" data-suite="${card.suite}" data-rank="${card.rank}">
           <div class="upperleft">${card.suite}</div>
           <div>${card.rank}</div>
           <div class="lowerright">${card.suite}</div>
@@ -51,17 +53,39 @@ function drawGame(store) {
 }
 
 function updateStore(action) {
-  const updatedStore = { ...store, round: [...store.round] };
+  console.log(store);
+  // Get the selected card
+  const [selectedCard] = store.round.filter((card) => {
+    return (
+      card.suite === action.dataset.suite &&
+      card.rank.toString() === action.dataset.rank
+    );
+  });
+  // Get the index of the selected card
+  const indexOfSelectedCard = store.round.findIndex(
+    (card) =>
+      card.suite === action.dataset.suite &&
+      card.rank.toString() === action.dataset.rank
+  );
+  // Create a copy of the card in the current round
+  const updatedRound = [...store.round];
+  // Set played = true for the selected card in the current round
+  updatedRound[indexOfSelectedCard] = { ...selectedCard, played: true };
+  // Update the store
+  const updatedStore = { ...store, round: updatedRound };
+  console.log(updatedStore);
   switch (action) {
     case "card":
+      // find card
+      // set boolean played to true on found card
       // update state
       break;
     case "forward":
       // update state
       break;
     default:
-      console.log("Suite: ", action.dataset.suite);
-      console.log("Rank: ", action.dataset.rank);
+    // console.log("Suite: ", action.dataset.suite);
+    // console.log("Rank: ", action.dataset.rank);
   }
   return updatedStore;
 }
